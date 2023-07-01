a98af04035f064832fc9ff99f97bbdb82b5dee1b
ELSHNKHLL <elshnkhll@yahoo.com>
2023-07-01 11:23:43 -0400

    Update ffp.sh

--- RPListening/ffp.sh
+++ RPListening/ffp.sh
@@ -1,2 +1,1 @@
-ffplay -hide_banner -loglevel debug -protocol_whitelist pipe,file,udp,rtp -vn -nodisp -nostats -i roku.sdpffplay -hide_banner -loglevel debug -protocol_whitelist pipe,file,udp,rtp -vn -nodisp -nostats -i roku.sdp
-
+ffplay -hide_banner -loglevel debug -protocol_whitelist pipe,file,udp,rtp -vn -nodisp -nostats -i roku.sdp
